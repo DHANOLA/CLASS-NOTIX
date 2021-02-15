3a4245d56f4f97bd3aa93a2be7da8339fb49d786
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-15 19:32:28 +0530

    Create QUESTIO 6.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 1/QUESTIO 6.py
@@ -0,0 +1,1 @@
+print("Twinkle, Twinkle, little star,\n \t\t How I wonder What you are !   \t\t Up above the world so high, \t\t Like a diamond in the sky. \n Twinkle, Twinkle, little star, \n \t\t How I wonder What you are")
