00b59fd1fd27543c6620c48e9425e84df0f43ad0
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-17 22:03:52 +0530

    Create QUESTION 7.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 2/QUESTION 7.py
@@ -0,0 +1,18 @@
+input1=float(input("enter the number 1:"))
+
+input2=float(input("enter the number 2:"))
+
+print("number before swapping:\n number 1:",input1,"\n number 2:",input2)
+
+temp=float()
+
+temp=input1
+
+
+input1=input2
+
+
+input2=temp
+
+
+print("number after swapping:\n number 1:",input1,"\n number 2:",input2)
