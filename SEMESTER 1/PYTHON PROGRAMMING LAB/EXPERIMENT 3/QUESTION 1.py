9eb0a9428eeb7774166fe7802a03878281f979df
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-19 12:37:19 +0530

    Create QUESTION 1.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 3/QUESTION 1.py
@@ -0,0 +1,10 @@
+p=float(input("enter the principal amount: "))
+r=float(input("enter the rate: "))
+p=float(input("enter the time: "))
+
+
+si=(p*r*t)/100
+
+print("the simple interest is: ",si)
+
+
