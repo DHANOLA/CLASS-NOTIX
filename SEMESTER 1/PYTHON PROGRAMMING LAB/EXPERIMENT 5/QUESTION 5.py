1b75ef7b473f6e02e5e3faa07467119c131ed414
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-19 21:38:37 +0530

    Create QUESTION 5.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 5/QUESTION 5.py
@@ -0,0 +1,11 @@
+num = int(input("Enter number of values: "))  
+
+
+print("value after squaring") 
+for i in range(num):
+    n = int(input("Enter the number: "))  
+    print("value after squaring: ",n*n) 
+    
+    
+    
+    
