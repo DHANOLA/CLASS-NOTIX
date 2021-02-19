a1ecffae6948ddcfbdec95685555ee650f27048f
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-19 21:45:12 +0530

    Create QUESTION 6.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 5/QUESTION 6.py
@@ -0,0 +1,15 @@
+S1 = {"Red" ,"yellow", "orange" , "blue "}
+S2 = {"violet", "blue", "purple"}
+
+
+# union 
+print("Union :", S1 | S2) 
+  
+# intersection 
+print("Intersection :", S1 & S2) 
+  
+# difference 
+print("Difference :", S1 - S2) 
+  
+# symmetric difference 
+print("Symmetric difference :", S1 ^ S2) 
