6d70b1c9a07a913b44aa57c91f3adf9d73653f98
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-19 22:19:38 +0530

    Create QUESTION 2.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 6/QUESTION 2.py
@@ -0,0 +1,14 @@
+l=list()
+
+
+for i in range(3):
+    num = int(input("Enter the number : ")) 
+    for j in range(11):
+        l.append(num*j)
+        
+        
+print("the list is: ",l)
+
+
+
+
