ac9e9ef9a2611224da583a462566f21d55507114
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-19 18:58:21 +0530

    Create QUESTION 6.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 3/QUESTION 6.py
@@ -0,0 +1,10 @@
+import math
+
+r=float(input("enter the radius of cylinder: "))
+
+h=float(input("enter the height of cylinder: "))
+
+vol=math.pi*math.pow(r,2)*h
+
+print("the volume of cylinder is: ",vol)
+
