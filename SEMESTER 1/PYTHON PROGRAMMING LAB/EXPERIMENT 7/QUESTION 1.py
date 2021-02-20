f713f93d2b3f8f6668729acb619da65e35ecf8d2
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-20 08:29:18 +0530

    Create QUESTION 1.py

---
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 7/QUESTION 1.py
@@ -0,0 +1,21 @@
+file=open(input("Enter the number of file: "))
+
+
+#A) COUNT NO OF NAMES
+
+
+count=0
+for name in file:
+    if name=="\n"
+        count+=1
+print("number of names: ",count)
+
+
+B) COUNT ALL NAMES STARTING WITH VOWEL
+
+
+count=0
+for name in file:
+    if name[0]=="\n"
+        count+=1
+C) FIND LONGEST NAME
