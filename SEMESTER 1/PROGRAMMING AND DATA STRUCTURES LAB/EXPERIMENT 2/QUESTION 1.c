3aab7be02e3ed89a36372b2bb38c9ec02f1d4181
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-21 19:26:52 +0530

    Create QUESTION 1.c

---
+++ SEMESTER 1/PROGRAMMING AND DATA STRUCTURES LAB/EXPERIMENT 2/QUESTION 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+      int num1,num2;
+    
+    printf("\nEnter two numbers:");
+    scanf("%d %d", &num1, &num2)
+    printf(" value of number 1=%d and number 2=%d before swapping", num1,num2);
+
+    i = i ^ k;
+    k = i ^ k;
+    i = i ^ k;
+    printf("value of i=%d k=%d after swapping",num1,num2);
+
+    return 0;
+}
