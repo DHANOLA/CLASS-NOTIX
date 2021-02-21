d7d7acf1a6f673e7d69158c0158b8aeda9b28030
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-21 19:16:21 +0530

    Create QUESTION 1.c

---
+++ SEMESTER 1/PROGRAMMING AND DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    
+    float h,b,area;
+    
+    
+    printf("\n\nEnter the height of the Triangle: ");
+    
+    
+    scanf("%f", &h);
+    
+    
+    printf("\n\nEnter the base of the Triangle: ");
+    
+    
+    scanf("%f", &b);
+
+
+area = .5*h*b;
+
+
+    printf("\n\n\nThe area of the triangle is: %f", area);
+
+
+    return 0;
+}
