cfaed27e5c329ca0c0b7707afc189486b019ecf5
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-26 21:31:47 +0530

    Create QUESTION 1.cpp

---
+++ SEMESTER 2/ADVANCED DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+
+int main(){
+
+float area=314,lenr=2,hrt=4;
+  
+  brdr=arae/lenr;
+  
+  recperi=2*lenr+2*brdr;
+    
+  sidesq=sqrt(area);
+
+sqperi=4*sidesq;
+  
+  
+
+}
