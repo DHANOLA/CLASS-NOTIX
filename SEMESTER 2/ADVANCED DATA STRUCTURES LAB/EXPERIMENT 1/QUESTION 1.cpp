73170fb5cbf83c841a0f8bd7f6ef6f8fbc03e62f
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-27 17:38:27 +0530

    Update QUESTION 1.cpp

--- SEMESTER 2/ADVANCED DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.cpp
+++ SEMESTER 2/ADVANCED DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+void max_of_four (int,int,int,int)
 
 int main(){
 
@@ -24,6 +25,41 @@ bse_tri=area/(.5*hi_tri);
   
   tri_peri=hi_tri+bse_tri+hypo_tri;
   
+circ_radii=sqrt((area)/(22/7));
   
-
+  circ_peri=2*(22/7)*circ_radii;
+  
+  max_of_four(rec_peri,sq_peri,tri_peri,circ_peri);
+  
+  return 0;
+  
 }
+
+   
+void max_of_four(float p,float q,float r,float s)
+{
+  if((p>q)&&(p>r)&&(p>s))
+  {
+    printf("grater perimetre among all the four is of rectangle  : %f",p);
+  }
+  else
+    {
+        if ((q>r) && (q>s))
+        {
+            printf("grater perimetre among all the four is of square : %f",q);
+        }
+        else
+        {
+            if (r>s)
+            {
+                printf("grater perimetre among all the four is of triangle : %f",r);
+            }
+            else
+            {
+                printf("grater perimetre among all the four is of circle : %f",s);
+
+            }
+        }
+    }
+}
+
