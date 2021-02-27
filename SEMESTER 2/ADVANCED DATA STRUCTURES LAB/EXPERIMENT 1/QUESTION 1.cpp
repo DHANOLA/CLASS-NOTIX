be35287f4955e649afc0eb02f427b5a4c3f1de09
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-02-27 14:16:14 +0530

    Update QUESTION 1.cpp

--- SEMESTER 2/ADVANCED DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.cpp
+++ SEMESTER 2/ADVANCED DATA STRUCTURES LAB/EXPERIMENT 1/QUESTION 1.cpp
@@ -7,15 +7,22 @@ using namespace std;
 
 int main(){
 
-float area=314,lenr=2,hrt=4;
+float area=314,len_rec=2,hi_tri=4;
   
-  brdr=arae/lenr;
+  rec_beadth=area/len_rec;
   
-  recperi=2*lenr+2*brdr;
+  rec_peri=2*len_rec+2*rec_breadth;
     
-  sidesq=sqrt(area);
+  side_sq=sqrt(area);
 
-sqperi=4*sidesq;
+sq_peri=4*sidesq;
+  
+bse_tri=area/(.5*hi_tri);
+  
+  
+  hypo_tri=sqrt((bse_tri*bse_tri)+(hi_tri*hi_tri));
+  
+  tri_peri=hi_tri+bse_tri+hypo_tri;
   
   
 
