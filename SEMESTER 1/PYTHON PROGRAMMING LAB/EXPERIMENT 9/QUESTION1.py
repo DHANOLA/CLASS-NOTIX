1f890fd10c8f47ce851735873719f7d9c9734d5c
RAHUL DHANOLA <rahuldhanola31@gmail.com>
2021-08-17 12:25:02 +0530

    Update QUESTION1.py

--- SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 9/QUESTION1.py
+++ SEMESTER 1/PYTHON PROGRAMMING LAB/EXPERIMENT 9/QUESTION1.py
@@ -1,7 +1,14 @@
 Class Student:
-  def __init__(self,name,sap,rollno):
+  
+  def setname(self,name):
     self.name=name
+    
+    
+   def setsap(self,sap):
     self.sap=sap
+    
+    
+   def setrollno(self,rollno):
     self.rollno=rollno
    
   
@@ -14,11 +21,20 @@ Class Student:
   def getrollno(self):
     return self.rollno
   
-student1=Student("Rahul",500074515,R177451248)
+student1=Student()
 
-student2=Student("Rohan",500074458,R177451854)
+student2=Student()
 
-student3=Student("Shivam",500078741,R177451965)
+student3=Student()
+
+print("Student 1: \nEnter Name:",student1.setname(),"\nEnter SAP ID:",student1.setsap(),"\nEnter Roll No:",student1.setrollno())
+
+
+print("Student 2: \nEnter Name:",student2.setname(),"\nEnter SAP ID:",student2.setsap(),"\nEnter Roll No:",student2.setrollno())
+
+
+print("Student 3: \nEnter Name:",student3.setname(),"\nEnter SAP ID:",student3.setsap(),"\nEnter Roll No:",student3.setrollno())
+
 
 print("Student 1: \nName:",student1.getname(),"\nSAP ID:",student1.getsap(),"\nRoll No:",student1.getrollno())
 
